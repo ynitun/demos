b0504e1439afc6ab280505d2e8918df149ff6caa
Tun Yin Ni <>
2024-01-26 14:33:52 +0800

    feature : draft of UI layout

--- wftc/src/App.tsx
+++ wftc/src/App.tsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+import { FC } from "react";
+interface AppProps {
+  title: string;
 }
 
-export default App
+const App: FC<AppProps> = ({ title }) => {
+  return (
+    <form>
+      <div className="container">
+        <div>
+          <h3>{title}</h3>
+        </div>
+        <div>
+          <div className="datetime">
+            <label>Date</label>
+            <input name="date" type="date" />
+          </div>
+          <div className="datetime">
+            <label>Time</label>
+            <input name="time" type="time" />
+          </div>
+        </div>
+        <div>
+          <div className="locations">
+            <select name="locations" size={6}>
+              <option value="1"> Location 1 </option>
+              <option value="2"> Location 2 </option>
+              <option value="3"> Location 3 </option>
+              <option value="4"> Location 4 </option>
+              <option value="5"> Location 5 </option>
+              <option value="6"> Location 6 </option>
+            </select>
+          </div>
+          <div className="weather">
+            <textarea name="weather" readOnly placeholder="Weather" />
+          </div>
+        </div>
+        <div>
+          <div className="image">
+            <img src="" alt="Screenshot" />
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default App;
